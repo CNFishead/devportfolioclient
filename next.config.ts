234f601b94a8412518922dca23999f46947679aa
Austin Howard <cnfishead@hotmail.com>
2025-05-13 02:14:36 -0400

    env

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    ENV: "production",
+    // ENV: 'development',
+    // API_URL: 'https://api.howardapi.com/api/v1',
+    API_URL: "https://api.austinhoward.dev/api/v1",
+  },
   images: {
     remotePatterns: [
       {
